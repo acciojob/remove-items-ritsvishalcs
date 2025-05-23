e0c2c9ef0a0a992580f70a88b575b022e4a91eac
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-23 16:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeColor() {
+    // Get the select element
+    const select = document.getElementById("colorSelect");
+
+    // Remove the selected option
+    select.remove(select.selectedIndex);
+}
